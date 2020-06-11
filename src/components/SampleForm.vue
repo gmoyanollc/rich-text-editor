5a536c8e537a16e7790d0e387c2052cfd427c968
g <g@vm-51.g>
2020-06-11 16:31:35 -0700

    border styling

--- src/components/SampleForm.vue
+++ src/components/SampleForm.vue
@@ -17,12 +17,15 @@
         v-model="lastName"
       /><br /><br />
       <slot />
-      <div id="richTextEditorDiv" class="form-group">
-        <label for="richTextEditor" class="text-left" style="margin: 0.5rem;">Rich Text Editor</label>
+      <label for="richTextEditor" class="text-left">Rich Text Editor</label>
+      <div
+        id="richTextEditorDiv"
+        class="form-group rich-text-editor"
+        onmouseout="this.classList.toggle('rich-text-editor-mouseover');"
+        onmouseover="this.classList.toggle('rich-text-editor-mouseover');"
+      >
         <RichTextEditor
           id="richTextEditor"
-          class="border rounded text-left border-color"
-          style="border-color: rgb(206, 212, 218);"
           v-bind:content-dispatch="RICH_TEXT_EDITOR.DISPATCH_NAME"
           v-bind:content-property="RICH_TEXT_EDITOR.PROPERTY_NAME"
           v-bind:content-style="RICH_TEXT_EDITOR.STYLE"
@@ -93,12 +96,32 @@ export default {
 </script>
 
 <style>
-#richTextEditorDiv {
-  border-color: rgb(182, 181, 181);
-  border-radius: 5px;
-  border-style: solid;
-  border-width: thin;
+.rich-text-editor {
   position: relative;
   width: 100%;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  -webkit-transition: border-color 0.15s ease-in-out,
+    -webkit-box-shadow 0.15s ease-in-out;
+  transition: border-color 0.15s ease-in-out,
+    -webkit-box-shadow 0.15s ease-in-out;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+    -webkit-box-shadow 0.15s ease-in-out;
+}
+.rich-text-editor-mouseover {
+  color: #495057;
+  background-color: #fff;
+  border-color: #80bdff;
+  outline: 0;
+  -webkit-box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
 }
 </style>
